47b8263c9be182a96f1970b908029c96cae11cf5
salino3 <programacionflavio@gmail.com>
2023-12-15 13:27:09 +0100

    imported class

--- src/app.js
+++ src/app.js
@@ -1,31 +1,5 @@
-document.addEventListener("DOMContentLoaded", function () {
-  class FormField {
-    constructor(labelText, inputType, inputId, inputName) {
-      this.labelText = labelText;
-      this.inputType = inputType;
-      this.inputId = inputId;
-      this.inputName = inputName;
-    }
+import { FormField } from "./js/form-field.js";
 
-    render() {
-      let formGroup = document.createElement("div");
-      formGroup.classList.add("form-group");
-
-      let label = document.createElement("label");
-      label.textContent = this.labelText;
-      label.setAttribute("for", this.inputId);
-
-      let input = document.createElement("input");
-      input.setAttribute("type", this.inputType);
-      input.setAttribute("id", this.inputId);
-      input.setAttribute("name", this.inputName);
-
-      formGroup.appendChild(label);
-      formGroup.appendChild(input);
-
-      return formGroup;
-    }
-  }
 
   // Uso de la clase para crear instancias y agregar campos al formulario
   let nameField = new FormField("Name", "text", "name", "name");
@@ -34,9 +8,8 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // Supongamos que tienes un formulario con el id "myForm" en tu HTML
   let form = document.getElementById("myForm");
-
   // Agrega los campos al formulario
   form.appendChild(nameField.render());
   form.appendChild(surnameField.render());
   form.appendChild(emailField.render());
-});
+
